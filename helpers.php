6d5fc4e33788f0ed293a47091db662edeff2866e
Bram Veerman <bram.veerman@gmail.com>
2018-03-21 09:16:57 +0100

    Now accepts relations, class constants, etc.

--- helpers.php
+++ helpers.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Relation;
 use TheJawker\Interrogator\Interrogator;
 use Illuminate\Database\Eloquent\Builder;
 
@@ -8,11 +10,23 @@ if (!function_exists('interrogate')) {
     /**
      * Shortcut to the Interrogator.
      *
-     * @param Builder $builder The Model builder to interrogate.
+     * @param Builder|Relation|string $builder The Model builder to interrogate.
      * @return Interrogator
      */
-    function interrogate(Builder $builder)
+    function interrogate($builder)
     {
+        if (is_string($builder)) {
+            $builder = new $builder;
+        }
+
+        if ($builder instanceof Model) {
+            $builder = $builder::query();
+        }
+
+        if ($builder instanceof Relation) {
+            $builder = $builder->getQuery();
+        }
+
         return new Interrogator($builder);
     }
 
